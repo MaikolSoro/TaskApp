c1afd15b7e10a9ab1b47de31b20d3da23e23a53b
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-09-27 13:18:25 -0600

    Create model class

--- app/src/main/java/com/example/taskapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/taskapp/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val MediumGray = Color(0xFF9C9C9C)
+
+val LowPriorityColor = Color(0xFF00C980)
+val MediumPriorityColor = Color(0xFFFFC114)
+val HighPriorityColor = Color(0XFFFF4646)
+val NonePriorityColor = MediumGray
---
+++ app/src/main/java/com/example/taskapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.taskapp.util
+
+object Constants {
+
+    const val DATABASE_TABLE = "todo_table"
+    const val DATABASE_NAME = "todo_database"
+}
