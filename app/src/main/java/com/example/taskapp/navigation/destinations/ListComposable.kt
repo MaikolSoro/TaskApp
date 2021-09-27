a00fb33cb3e7e0e27d32734e06276ad801d74bac
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-09-27 17:18:00 -0600

    LÃist Screen - FAB

--- app/src/main/java/com/example/taskapp/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/example/taskapp/navigation/destinations/ListComposable.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
+import com.example.taskapp.ui.screens.list.ListScreen
 import com.example.taskapp.util.Constants.LIST_ARGUMENT_KEY
 import com.example.taskapp.util.Constants.LIST_SCREEN
 
@@ -16,6 +17,6 @@ fun NavGraphBuilder.listComposable(
             type = NavType.StringType
         })
     ){
-
+        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
     }
 }
