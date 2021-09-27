7ac876d180e235a0ba3e3a1d5e9d443f63e2ea31
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-09-27 12:28:24 -0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.taskapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskapp.ui.theme.TaskAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TaskAppTheme {
+        Greeting("Android")
+    }
+}
