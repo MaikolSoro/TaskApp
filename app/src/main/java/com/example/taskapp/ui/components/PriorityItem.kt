873b7727e52528b11f2f8dcbe90b2b576cf629aa
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-09-27 19:42:37 -0600

    ListScreen - List App Bar Actions

---
+++ app/src/main/java/com/example/taskapp/ui/components/PriorityItem.kt
@@ -0,0 +1,41 @@
+package com.example.taskapp.ui.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskapp.data.models.Priority
+import com.example.taskapp.ui.theme.LARGE_PADDING
+import com.example.taskapp.ui.theme.PRIORITY_INDICATOR_SIZE
+import com.example.taskapp.ui.theme.Typography
+
+@Composable
+fun PriorityItem(priority: Priority) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Canvas(modifier = Modifier.size(PRIORITY_INDICATOR_SIZE)) {
+            drawCircle(color = priority.color)
+        }
+        Text(
+            modifier = Modifier.padding(start = LARGE_PADDING),
+            text = priority . name,
+            style = Typography.subtitle1,
+            color = MaterialTheme.colors.onSurface
+
+
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PriorityItemPreview() {
+    PriorityItem(priority = Priority.HIGH)
+}
